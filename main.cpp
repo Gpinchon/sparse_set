5c815806b5a1b90f0e37bb73b8033dea7d3add94
gpinchon <pinchon.guillaume@gmail.com>
2022-12-04 19:45:25 +0100

    add test code

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <sparse_set.hpp>
+
+#include <cassert>
+
+struct Transform {
+    std::array<float, 3> position{ 0, 0, 0 };
+};
+
+int main()
+{
+    auto sparseSet = new sparse_set<Transform, 65536>;
+    for (auto i = 0u; i < sparseSet->max_size(); ++i) {
+        sparseSet->insert(i).position[0] = i;
+    }
+    for (auto i = 0u; i < sparseSet->size(); ++i) {
+        assert(sparseSet->at(i).position[0] == i);
+    }
+    for (auto i = 0u; i < sparseSet->max_size(); ++i) {
+        if (i % 3) sparseSet->erase(i);
+    }
+    for (auto i = 0u; i < sparseSet->max_size(); ++i) {
+        if (i % 3) assert(!sparseSet->contains(i));
+        else assert(sparseSet->contains(i));
+    }
+    delete sparseSet;
+}
